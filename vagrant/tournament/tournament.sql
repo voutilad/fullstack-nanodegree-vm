3b4de0b9d44e44ff0f6565fb89c2d9af772bfc52
voutilad <voutilad@gmail.com>
2015-06-03 15:05:23 -0400

    some sql schema definition

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,27 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
+\c tournament;
 
+CREATE TABLE player (
+  id  int primary key not null,
+  name  text  not null,
+);
+
+CREATE TABLE tournament (
+  id  int primary key not null,
+  date  date DEFAULT CURRENT_DATE,
+  name  text
+)
+
+CREATE TABLE match (
+  id  int primary key not null,
+  home_player_id  int references player(id),
+  away_player_id  int references player(id),
+  home_score  int DEFAULT 0,
+  away_score  int DEFAULT 0,
+  winner  int references player(id),
+  round int,
+  tournament_id int references tournament(id),
+)
