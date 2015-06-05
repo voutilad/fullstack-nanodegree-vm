3bfb4c8a99f70f0bc63e2d5130323d1eb8f165c4
Dave Voutila <voutilad@gmail.com>
2015-06-05 07:52:17 -0400

    fixed bug in sql script. now works in creating db and tables.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,27 +6,30 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+\echo Creating database...
 CREATE DATABASE tournament;
 \c tournament;
 
+\echo Creating Player Table...
 CREATE TABLE player (
   id  int primary key not null,
-  name  text  not null,
+  name  text  not null
 );
 
+\echo Creating Tournament Table...
 CREATE TABLE tournament (
   id  int primary key not null,
   date  date DEFAULT CURRENT_DATE,
   name  text
-)
+);
 
+\echo Creating Match Table...
 CREATE TABLE match (
   id  int primary key not null,
-  home_player_id  int references player(id),
-  away_player_id  int references player(id),
   home_score  int DEFAULT 0,
   away_score  int DEFAULT 0,
   winner  int references player(id),
+  loser int references player(id),
   round int,
-  tournament_id int references tournament(id),
-)
+  tournament_id int references tournament(id)
+);
